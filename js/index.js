db00929d0cc1b457dbc336cfc697d30930144473
VILSON GARCIA <134901360+VILSON-DEV@users.noreply.github.com>
2023-07-13 05:11:36 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function clicar(){
+    var texto = document.getElementById('clicar')
+    texto.style.color = "rgb(54, 16, 116)";
+    texto.innerHTML = "PRODUTOS NOVOS E USADOS";
+}
+function tirar(){
+    var texto = document.getElementById('clicar')
+    texto.style.color = "black";
+    texto.innerHTML= "SSD Kingston 480GB por R$250,00";
+}
+ 
